e52ee744b6e489c2b382a6a81f9559f2e9e88000
aurelieaurelie <aurelie1112@gmail.com>
2014-06-29 00:53:37 +0200

    fourth

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-$(document).ready(function(e) {
+$(document).ready(function() {
 	$('.container-home form').hide();
-	$('.btn').click(function() {
+	$('.btn').click(function(e) {
     var homeForm = $(this).data('id');
     $('.container-home form').not('#' + homeForm).hide();
     $('#' + homeForm).show();
         e.preventDefault();
+  });
+
+	$('.evol-block').css('display','none');
+      $('.evol-nav a').click(function(f) {
+  		//$(this).toggleClass('clicked');
+	    var evolBlock = $(this).data('id');
+	    $('.evol-block').not('#' + evolBlock).css('display','none');
+	    $('#' + evolBlock).css('display','block');
+	    console.log('ouin');
+	      f.preventDefault();
   });
 	
 });
